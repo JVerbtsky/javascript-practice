142228c31342596dc02c9bef29d942fc5b556520
Joseph Verbitsky <95263085+JVerbtsky@users.noreply.github.com>
2022-02-28 18:12:05 -0800

    take-home challenge

---
+++ js/script.js
@@ -0,0 +1,30 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"; //string
+Alphabet = "abcdefghijklmnopqrstuvwxyza"; //string case sensitive, entirely different variable
+
+numbers = 1234567890; // number
+
+name = "Vinson";
+
+yes = true; // booleans
+no = false;
+
+yes = true; // boolean (true or false)
+no = false; // boolean (decision or condition)
+
+// Complex/ Compound datatypes
+
+
+person = { // object
+    name: "Joseph",
+    age: 29,
+    isMarried: false,
+};
+
+food = [ //array 
+    "Ramen",
+    "Roasted Vegetables",
+    "Izakaya"
+];
+
+console.log(person);
+console.log(food);
